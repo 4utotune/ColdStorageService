6c8391a3fa6e6441925467fabb706eed72ff7b56
4utotune <71821593+4utotune@users.noreply.github.com>
2023-06-29 11:27:41 +0200

    delete prova

--- prova/prova.pl
+++
@@ -1,7 +0,0 @@
-%====================================================================================
-% prova description   
-%====================================================================================
-context(ctx_prova, "127.0.0.1",  "TCP", "8020").
-context(ctx_signora, "localhost",  "TCP", "11800").
- qactor( signore, ctx_signora, "it.unibo.signore.Signore").
-  qactor( signora, ctx_prova, "it.unibo.signora.Signora").
